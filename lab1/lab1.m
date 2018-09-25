9c7b0223aa6b91d7c9b7c341d38cb325cab0449c
vincentzryoung <vincent.zr.young@hotmail.com>
2018-09-24 23:13:40 -0400

    Corrected Lab 1 Sec 5 gamma code, added programmatic image saving

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -60,6 +60,7 @@ lena_img = imread('lena.tiff');
 lena_gray = rgb2gray(lena_img); 
 
 lena_gray = double(lena_gray)/255;
+imwrite(lena_gray, 'DerivedImgs/lena_gray.tiff', 'tiff');
 
 h1 = (1/6)*ones(1,6);
 h2 = h1';
@@ -70,23 +71,27 @@ lena_h2 = conv2(lena_gray, h2);
 lena_h3 = conv2(lena_gray, h3);
 
 figure, montage([lena_gray lena_h1]);
+imwrite(lena_h1, 'DerivedImgs/lena_h1.tiff', 'tiff');
 title('h1');
 
 figure, imshow(lena_h2);
+imwrite(lena_h2, 'DerivedImgs/lena_h2.tiff', 'tiff');
 title('h2');
 
 figure, montage([lena_gray lena_h3]);
+imwrite(lena_h3, 'DerivedImgs/lena_h3.tiff', 'tiff');
 title('h3');
 
 %% Section 5 - Point Operations for Image Enhancement
 clear all
 close all
-tire = imread('original tire.tif');
+tire = imread('tire.tif');
+cur_dir = pwd;
+out_dir = 'DerivedImgs\Sec5';
+cd(out_dir)
 
 figure, imhist(tire)
 title('Tire Hist')
-
-
 
 class_name = class(tire); 
 max_f = double(intmax(class_name)); 
@@ -97,31 +102,44 @@ title('Tire and Negative Tire Imgs');
 figure, imhist(neg_img)
 title('Negative Tire Hist')
 
-tire_db = double(tire); 
+tire_db = double(tire)./max_f;
+
 gamma_tire_lo = tire_db.^0.5;
-gamma_tire_lo = uint8(gamma_tire_lo);  
+gamma_tire_lo = uint8(gamma_tire_lo*max_f);  
 
 gamma_tire_hi = tire_db.^1.3;
-gamma_tire_hi = uint8(gamma_tire_hi); 
+gamma_tire_hi = uint8(gamma_tire_hi*max_f); 
 
-figure, montage([gamma_tire_lo gamma_tire_hi]);
-title('Gamma 0.5 and Gamma 1.3'); 
+figure, montage([tire gamma_tire_lo gamma_tire_hi]);
+title('Original Image, Gamma 0.5 and Gamma 1.3'); 
+saveas(gcf, 'Tire_Gamma_Montage.png');
+
+figure, imhist(gamma_tire_lo);
+title('Lo Gamma Tire');
+saveas(gcf, 'Tire_Lo_Hist.png');
+
+figure, imhist(gamma_tire_hi);
+title('Hi Gamma Tire');
+saveas(gcf, 'Tire_Hi_Hist.png');
 
 figure, histeq(tire);
 title('Eq Tire');
+
 figure, histeq(neg_img);
 title('Eq Negative Tire');
+
 figure, histeq(gamma_tire_lo);
 title('Lo Gamma Tire');
+
 figure, histeq(gamma_tire_hi);
 title('Hi Gamma Tire');
 
 figure, imhist(histeq(gamma_tire_lo));
 title('Lo Gamma Eq Tire');
+
 figure, imhist(histeq(gamma_tire_hi));
 title('Hi Gamma Eq Tire');
 
-figure, imhist(gamma_tire_lo);
-title('Lo Gamma Tire');
-figure, imhist(gamma_tire_hi);
-title('Hi Gamma Tire');
+
+
+cd(cur_dir)
